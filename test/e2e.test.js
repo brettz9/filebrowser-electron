31b6dedae88d7faf590d7e19d2cfd32f4a59e45a
Brett Zamir <brettz9@yahoo.com>
2025-11-29 22:30:35 -0700

    test grouping

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -14,50 +14,54 @@ test.afterEach(async () => {
   return await coverage(app);
 });
 
-test('Successfully launches the app with @playwright/test.', async () => {
-  // See https://playwright.dev/docs/api/class-electronapplication for ElectronApplication documentation.
-  const {appPath, isPackaged} = await app.electron.evaluate(({
-    app: application
-  }) => {
-    return {
-      appPath: application.getAppPath(),
-      isPackaged: application.isPackaged
-    };
+test.describe('main', () => {
+  test('Successfully launches the app with @playwright/test.', async () => {
+    // See https://playwright.dev/docs/api/class-electronapplication for ElectronApplication documentation.
+    const {appPath, isPackaged} = await app.electron.evaluate(({
+      app: application
+    }) => {
+      return {
+        appPath: application.getAppPath(),
+        isPackaged: application.isPackaged
+      };
+    });
+
+    expect(appPath.endsWith('src')).toBe(true);
+    expect(isPackaged).toBe(false);
+
+    const initialScreenshotPath = 'test/screenshots/initial.png';
+
+    const window = await app.electron.firstWindow();
+    await window.screenshot({path: initialScreenshotPath});
+
+    // eslint-disable-next-line n/no-sync -- Non-deprecated
+    expect(existsSync(initialScreenshotPath)).toBe(true);
+
+    // Which title is this as its not being found?
+    // expect(await window.title()).toBe('Filebrowser');
   });
 
-  expect(appPath.endsWith('src')).toBe(true);
-  expect(isPackaged).toBe(false);
+  test('handles activate event', async () => {
+    // See https://playwright.dev/docs/api/class-electronapplication for ElectronApplication documentation.
+    await app.electron.evaluate(({
+      app: application
+    }) => {
+      application.emit('activate');
+    });
 
-  const initialScreenshotPath = 'test/screenshots/initial.png';
-
-  const window = await app.electron.firstWindow();
-  await window.screenshot({path: initialScreenshotPath});
-
-  // eslint-disable-next-line n/no-sync -- Non-deprecated
-  expect(existsSync(initialScreenshotPath)).toBe(true);
-
-  // Which title is this as its not being found?
-  // expect(await window.title()).toBe('Filebrowser');
-});
-
-test('handles activate event', async () => {
-  // See https://playwright.dev/docs/api/class-electronapplication for ElectronApplication documentation.
-  await app.electron.evaluate(({
-    app: application
-  }) => {
-    application.emit('activate');
+    // You can then assert on the expected behavior after activation
+    // For example, if activation brings a window to the front:
+    const mainWindow = await app.electron.firstWindow();
+    expect(await mainWindow.evaluate(() => document.hasFocus())).toBe(true);
   });
-
-  // You can then assert on the expected behavior after activation
-  // For example, if activation brings a window to the front:
-  const mainWindow = await app.electron.firstWindow();
-  expect(await mainWindow.evaluate(() => document.hasFocus())).toBe(true);
 });
 
-test('successfully finds the basic elements of the page', async () => {
-  expect(await app.main.locator('i').textContent()).toBe(
-    'Waiting for activation...'
-  );
+test.describe('renderer', () => {
+  test('successfully finds the basic elements of the page', async () => {
+    expect(await app.main.locator('i').textContent()).toBe(
+      'Waiting for activation...'
+    );
 
-  expect(await app.main.locator('i')).toBeHidden();
+    expect(await app.main.locator('i')).toBeHidden();
+  });
 });
